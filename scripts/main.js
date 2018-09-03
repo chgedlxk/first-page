0561e2ef4d4cb9b566a30aef756a1b184c464589
chgedlxk <chgedlxk@gmail.com>
2018-09-03 02:14:33 -0400

    Add a personalized welcome message

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,24 @@ myImage.onclick = function() {
 		myImage.setAttribute('src', 'images/razorback.jpg')
 	}
 }
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h2');
+var headingContent = myHeading.textContent;
+
+function setUserName() {
+	var myName = prompt('Please enter your name');
+	localStorage.setItem('name', myName);
+	myHeading.textContent = myName + " is getting a " + headingContent;
+}
+
+if (!localStorage.getItem('name')) {
+	setUserName();
+} else {
+	var storedName = localStorage.getItem('name');
+	myHeading.textContent = storedName + " is getting a " + headingContent;
+}
+
+myButton.onclick = function() {
+	setUserName();
+}
