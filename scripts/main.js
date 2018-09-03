489930abe63b77de3d87465f4344ad57c1fb155a
chgedlxk <chgedlxk@gmail.com>
2018-09-03 01:46:32 -0400

    Add an image changer

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,10 @@
-var myHeading = document.querySelector('h2');
-myHeading.textContent = 'Hello World';
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+	var mySrc = myImage.getAttribute('src');
+	if (mySrc === 'images/razorback.jpg') {
+		myImage.setAttribute('src', 'images/razorback1.jpg')
+	} else {
+		myImage.setAttribute('src', 'images/razorback.jpg')
+	}
+}
